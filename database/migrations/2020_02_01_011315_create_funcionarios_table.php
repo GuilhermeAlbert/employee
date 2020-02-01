629c1d574c646d9db808acca7ac4bc29e283f5e7
Guilherme Albert <guilhermealbert14@gmail.com>
2020-01-31 22:31:15 -0300

    Make the employee migrations

---
+++ database/migrations/2020_02_01_011315_create_funcionarios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFuncionariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('documento');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('cargo');
+            $table->string('salario');
+            $table->string('genero');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+}
