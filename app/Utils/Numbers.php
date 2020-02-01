5c85ebdaa5759f98643a4a36bcb93ae653a06fd9
Guilherme Albert <guilhermealbert14@gmail.com>
2020-01-31 23:32:44 -0300

    Resources and index method working

---
+++ app/Utils/Numbers.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Utils;
+
+/*
+ * Number utils
+ *
+ * Use method: Numbers::ONE;
+ */
+class Numbers
+{
+    const ONE       = 1;
+    const TWO       = 2;
+    const THREE     = 3;
+    const FOUR      = 4;
+    const FIVE      = 5;
+    const SIX       = 6;
+    const SEVEN     = 7;
+    const EIGHT     = 8;
+    const NINE      = 9;
+    const TEN       = 10;
+    const ELEVEN    = 11;
+    const TWELVE    = 12;
+    const THIRTEEN  = 13;
+    const FOURTEEN  = 14;
+    const FIFTEEN   = 15;
+    const SIXTEEN   = 16;
+    const SEVENTEEN = 17;
+    const EIGHTEEN  = 18;
+    const NINETEEN  = 19;
+    const TWENTY    = 20;
+}
